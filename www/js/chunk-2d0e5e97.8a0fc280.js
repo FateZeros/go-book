7f1a3c7235d24372d483decf506ab026143387f9
yejf <490293266@qq.com>
2020-05-23 12:15:17 +0800

    update

---
+++ www/js/chunk-2d0e5e97.8a0fc280.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5e97"],{9703:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("\n  Not Found\n")])},c=[],l=t("2877"),o={},r=Object(l["a"])(o,u,c,!1,null,null,null);e["default"]=r.exports}}]);
---
+++ www/precache-manifest.027eaa2f0fbb4a3f3a25f86d8d557295.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2bf33c001c0cce0d5a46b9b0d970754",
+    "url": "/img/a10.e2bf33c0.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7a243e410efcae8badf14e38a36b15ba",
+    "url": "/img/page5aks-f.7a243e41.png"
+  },
+  {
+    "revision": "5472885e1f6fdd50f1e3",
+    "url": "/js/chunk-1fe81c64.26db4f52.js"
+  },
+  {
+    "revision": "f171c69ca6c3e6cd9adb",
+    "url": "/js/chunk-2d0c04a8.239b7ff7.js"
+  },
+  {
+    "revision": "419cead2eceae2c9df69",
+    "url": "/js/chunk-2d0c4f96.925fc543.js"
+  },
+  {
+    "revision": "7c5147e680fb876b829a",
+    "url": "/js/chunk-2d0d09f1.b87688d8.js"
+  },
+  {
+    "revision": "3e2a1033958a486b2c5d",
+    "url": "/js/chunk-2d0ddf45.3bd102c6.js"
+  },
+  {
+    "revision": "1b4a5ce229cde206c4f0",
+    "url": "/js/chunk-2d0e5e97.8a0fc280.js"
+  },
+  {
+    "revision": "b38690f8fcf8ee66cccb",
+    "url": "/js/chunk-2d210b9d.375eded8.js"
+  },
+  {
+    "revision": "38933945805dc380229a",
+    "url": "/js/chunk-2d21b387.ce582f5a.js"
+  },
+  {
+    "revision": "8693b998a3e1b154d52c",
+    "url": "/js/chunk-2d2380e4.d6ff36a3.js"
+  },
+  {
+    "revision": "fd923d8ba79a2097aacf",
+    "url": "/js/chunk-5dde4a9e.e3ddfba8.js"
+  },
+  {
+    "revision": "b9800836fbb55a0aa6b3",
+    "url": "/js/chunk-64483f7b.9aee28ae.js"
+  },
+  {
+    "revision": "acf06677b4212b38cf0b",
+    "url": "/js/chunk-74b4ef7a.feed6f73.js"
+  },
+  {
+    "revision": "dae42a661927ac740b12",
+    "url": "/js/chunk-cfb321ec.eccf30ea.js"
+  },
+  {
+    "revision": "0ae9a0aa5938ea0aaa2f",
+    "url": "/js/chunk-vendors.7fb87bd1.js"
+  },
+  {
+    "revision": "eec1dd2298ef16d60ff6",
+    "url": "/js/index.6a4dc500.js"
+  },
+  {
+    "revision": "96eba3a581f011bcb72d79c5a0d07235",
+    "url": "/media/win.96eba3a5.wav"
+  },
+  {
+    "revision": "fac93a108a6bc13ed9b07fbd8acdc936",
+    "url": "/media/one.test.fac93a10.mp3"
+  },
+  {
+    "revision": "b1ab20a6e975e69164117e3f14f4c295",
+    "url": "/media/two_star.b1ab20a6.mp3"
+  },
+  {
+    "revision": "4807492edc6ef3626eebc4903dc9b15e",
+    "url": "/media/error1.4807492e.wav"
+  },
+  {
+    "revision": "b850034c11ceed4fd2a8f20ba3d57ffe",
+    "url": "/media/sha.b850034c.wav"
+  },
+  {
+    "revision": "e0c020a23d48e4728140b73cac86d4d6",
+    "url": "/media/mouth.e0c020a2.mp3"
+  },
+  {
+    "revision": "238c601cf9d60a50432b497ca5f825e3",
+    "url": "/media/error2.238c601c.wav"
+  },
+  {
+    "revision": "7d9a84aed68b02b288b0f0386c91a1c8",
+    "url": "/media/Touch_your_nose.7d9a84ae.mp3"
+  },
+  {
+    "revision": "c2dea2c78eb9cfdbde343fbf9b55d380",
+    "url": "/media/win2.c2dea2c7.wav"
+  },
+  {
+    "revision": "8d95995297c5f9864866c2ef198e898c",
+    "url": "/media/error.8d959952.wav"
+  },
+  {
+    "revision": "500753cddd22a6781abf4d23a9b2c69a",
+    "url": "/media/light.500753cd.wav"
+  },
+  {
+    "revision": "bf4b84d5fa6bbdb4c856a039fd84e925",
+    "url": "/media/ear.bf4b84d5.mp3"
+  },
+  {
+    "revision": "fac93a108a6bc13ed9b07fbd8acdc936",
+    "url": "/media/one.fac93a10.mp3"
+  },
+  {
+    "revision": "77548f2b05108ad66c5e55f6d9947b17",
+    "url": "/media/one_star.77548f2b.mp3"
+  },
+  {
+    "revision": "d316e095a83d55fe3e1b",
+    "url": "/js/chunk-2d0aa90d.0f0831de.js"
+  },
+  {
+    "revision": "170616f828da3355ada4113d2bdf91bb",
+    "url": "/media/three_star.170616f8.mp3"
+  },
+  {
+    "revision": "ce01ff8e6676ff4b546cd26de6f114ec",
+    "url": "/img/play.ce01ff8e.svg"
+  },
+  {
+    "revision": "de4e728dd86ba2e56eb6",
+    "url": "/js/chunk-01a76680.a5b9fb4e.js"
+  },
+  {
+    "revision": "c72d66794a1d32d28449",
+    "url": "/js/chunk-1c3a766d.449faa39.js"
+  },
+  {
+    "revision": "fbd2dc70c780ce4cc39bbfeb33a29850",
+    "url": "/img/bg-music.fbd2dc70.svg"
+  },
+  {
+    "revision": "cad326eef1971fe1259eb40e7194d922",
+    "url": "/img/microphone_click2.cad326ee.png"
+  },
+  {
+    "revision": "d44214668fb2441a5de02c8db32e1a47",
+    "url": "/img/mouth_block.d4421466.png"
+  },
+  {
+    "revision": "b7c39aca3a3b4f54d19ebe956fffd990",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24697c540a1f0fc67c29ce796bcace74",
+    "url": "/img/nose_shadow.24697c54.png"
+  },
+  {
+    "revision": "ea50db2942785ff2f5c042bb610c6e0d",
+    "url": "/img/right-light.ea50db29.png"
+  },
+  {
+    "revision": "7b03b09a38bd4768d5023f136f07f6f7",
+    "url": "/img/page5aks-t.7b03b09a.png"
+  },
+  {
+    "revision": "e2bf33c001c0cce0d5a46b9b0d970754",
+    "url": "/img/one.e2bf33c0.png"
+  },
+  {
+    "revision": "ead8e4426121572aa53c70d3be22e19f",
+    "url": "/img/microphone_lv.ead8e442.png"
+  },
+  {
+    "revision": "1a43f26f97b36e409a06456dc64e67ae",
+    "url": "/img/microphone_click3.1a43f26f.png"
+  },
+  {
+    "revision": "b27edf47f10e2da6cb50630326dc8a58",
+    "url": "/img/sound.b27edf47.png"
+  },
+  {
+    "revision": "d14a252390f495be8f143270a9879e28",
+    "url": "/img/paint_box.d14a2523.png"
+  },
+  {
+    "revision": "d6bfd94876ac7e882dd4f2b7612fbcbf",
+    "url": "/img/mouth.d6bfd948.png"
+  },
+  {
+    "revision": "7f976f68ea520c7d177c8033a22892b6",
+    "url": "/img/nose_block.7f976f68.png"
+  },
+  {
+    "revision": "cc9abaa1429cb568c78f1c3e5e9123d5",
+    "url": "/img/two.cc9abaa1.png"
+  },
+  {
+    "revision": "07c98ad01ec3821447f5b288c6d681a3",
+    "url": "/img/three.07c98ad0.png"
+  },
+  {
+    "revision": "a4c34760cf68e86954b3117941878ea9",
+    "url": "/img/light-bg.a4c34760.png"
+  },
+  {
+    "revision": "3de427d60aedcf583007ecd83eeaa622",
+    "url": "/img/a10.3de427d6.png"
+  },
+  {
+    "revision": "c4592edc60bd03e98232807849cdff89",
+    "url": "/img/ear.c4592edc.png"
+  },
+  {
+    "revision": "a17011bfc43d1135658c51a4ceb46a0b",
+    "url": "/img/left-light.a17011bf.png"
+  },
+  {
+    "revision": "1d0faf1f39abbe2bad0c10d0442d18c2",
+    "url": "/img/demo1-light.1d0faf1f.png"
+  },
+  {
+    "revision": "48eb1ed5b07cb75ad7ddc70227211474",
+    "url": "/img/microphone.48eb1ed5.png"
+  },
+  {
+    "revision": "20ef42b5ef3bc07f42c8989fa536268d",
+    "url": "/img/demo1.20ef42b5.png"
+  },
+  {
+    "revision": "2ae2495d07a00c5624ec4d0d48f377ad",
+    "url": "/img/logo-bg.2ae2495d.png"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7a7083aa1ae47ee140a5addbf7ed7361",
+    "url": "/img/microphone_click1.7a7083aa.png"
+  },
+  {
+    "revision": "cb1d21c6ef31cd84e45de8621be1a8de",
+    "url": "/img/demo2.cb1d21c6.png"
+  },
+  {
+    "revision": "c1fc064f887b8e743909fad1998a551c",
+    "url": "/img/acad-picture-placeholder.c1fc064f.png"
+  },
+  {
+    "revision": "365b869df1111c1157769b823f690340",
+    "url": "/img/demo3.365b869d.png"
+  },
+  {
+    "revision": "ceb878fcd028a93406d9dc7893d39a8b",
+    "url": "/img/Objectives_box.ceb878fc.png"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "8eb03871e6046162efce5f2cdf5fa849",
+    "url": "/fonts/Cooper Black.8eb03871.ttf"
+  },
+  {
+    "revision": "a50f9c96a76356e3d01013e0b042989f",
+    "url": "/fonts/Comic Sans MS.a50f9c96.ttf"
+  }
+];
